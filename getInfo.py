b3e470a52948e20daa8b4962491939f230c73dbd
Degover <chaors2000@gmail.com>
2018-10-23 17:55:58 -0300

    getInfo.py finalizado
    Adicionado partes no getInfo.py que encontrara o artista, a letra e a imagem do album da musica.
    Também, foram retirados as funções relacionadas ao Wikipedia, já que não serão mais usadas.

--- getInfo.py
+++ getInfo.py
@@ -25,13 +25,77 @@ def searchGoogle(name):
     #Acha os elementos da pagina e os coloca no dicionario
     elems = soup.select(u'div div div div div div span')
     detailsDic = {'Artista': None, 'Álbum': None,
-           'Data de lançamento': None, 'Gêneros': None}
+           'Data de lançamento': None, 'Gênero': None, 
+           'Imagem': None, 'Letra': None, 'Titulo':None}
+    #Testa se o elemento esta no dicionario
     for elemNum in range(len(elems)):
-        elem = elems[elemNum].getText()[:-2]
-        if elem in detailsDic.keys():
-            detailsDic[elem] = elems[(elemNum + 1)].getText()
+        elem1 = elems[elemNum].getText()[:-2]
+        elem2 = elems[elemNum].getText()[:-3]
+        if elem1 in detailsDic.keys():
+            detailsDic[elem1] = elems[(elemNum + 1)].getText()
+        elif elem2 in detailsDic.keys():
+            detailsDic[elem2] = elems[(elemNum + 1)].getText()
+            
+    #Retira o titulo da musica
+    title = name.replace(detailsDic['Artista'].lower(), '').replace('-', '')
+    #Formata o titulo
+    newTitle = ''
+    for word in title.split():
+        if len(word) > 2:
+            newTitle += word.title() + ' '
+        else:
+            newTitle += word  + ' '
+    newTitle = newTitle[:-1]
+    detailsDic['Titulo'] = newTitle       
+    #Acha os sites das letras
+    elems = soup.select(u'.r a')
+    for elem in elems:
+        site = elem['href'][7:].rsplit(r'&sa=')[0]
+        if r'www.letras.mus.br' in site:
+            detailsDic['Letra'] = searchLetras(site)
+
     
+    #Acha a imagem
+    #Faz o download da pagina de imagens
+    album = detailsDic['Artista'] + ' ' + detailsDic['Álbum'] + r'&tbm=isch'
+    res = requests.get(getGoogle(album))
+    res.raise_for_status()
+    #Procura as imagens
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    elems = soup.select(u'div a img')
+    imgUrl = elems[0].get('src')
+    #Salva a imagem no dicionario
+    res = requests.get(imgUrl)
+    res.raise_for_status()
+    detailsDic['Imagem'] = res.iter_content()
+        
     #Retorna o dicionario com os detalhes achados
     return(detailsDic)
+
+def searchLetras(site):
+    #Faz com que a HTML seja sem tradução
+    if r'traducao.html' in site:
+        site = site[:-14]
+        
+    #Faz o download da pagina
+    res = requests.get(site)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    
+    #Acha a letra da musica
+    elems = soup.select('div div div div div div article p')
+    
+    #Formata a letra em uma string
+    letra = ''
+    for parag in elems:
+        #Troca as marcações do HTML por espaços e linhas
+        paragStr = str(parag)[3:].replace(r'</p>', '\n\n').replace(r'<p>', '\n')
+        paragStr = paragStr.replace(r'<br/>', '\n').replace(r'</br>', 
+                                   '').replace(r'<br>', '\n')
+        #Adiciona na string
+        letra += paragStr
+    
+    #Retorna a letra
+    return letra
         
 searchGoogle('Jim Croce Time in a bottle')
